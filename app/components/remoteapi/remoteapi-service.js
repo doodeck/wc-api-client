b9c297efed08a210913870873510f73eea194b37
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-03-14 06:42:41 +0100

    Added workaround for JSONP

--- app/components/remoteapi/remoteapi-service.js
+++ app/components/remoteapi/remoteapi-service.js
@@ -11,7 +11,45 @@ angular.module('myApp.remoteapi', [])
       hostname = host;
     },
     getWSDL: function() {
-      return $http.get(hostname + '/wc-api/v2');
+      return $http({method: 'JSONP', url: hostname + '/wc-api/v2?_jsonp=JSON_CALLBACK'});
     }
   }
-}]);
+}])
+.config(function($httpProvider) { // nice: http://stackoverflow.com/questions/25400891/how-to-custom-set-angularjs-jsonp-callback-name
+  $httpProvider.interceptors.push('jsonpInterceptor');
+})
+.factory('jsonpInterceptor', function($timeout, $window) {
+  return {
+    'request': function(config) {
+      if (config.method === 'JSONP') {
+        var callbackId = angular.callbacks.counter.toString(36);
+        config.callbackName = 'angular_callbacks_' + callbackId;
+        config.url = config.url.replace('JSON_CALLBACK', config.callbackName);
+
+        $timeout(function() {
+          $window[config.callbackName] = angular.callbacks['_' + callbackId];
+        }, 0, false);
+      }
+
+      return config;
+    },
+
+    'response': function(response) {
+      var config = response.config;
+      if (config.method === 'JSONP') {
+        delete $window[config.callbackName]; // cleanup
+      }
+
+      return response;
+    },
+
+    'responseError': function(rejection) {
+      var config = rejection.config;
+      if (config.method === 'JSONP') {
+        delete $window[config.callbackName]; // cleanup
+      }
+
+      return $q.reject(rejection);
+    }
+  };
+});
