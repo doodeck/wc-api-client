dcb457bf49f8acaf6202b5d72f64820f9ccff7ab
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-03-14 22:59:48 +0100

    implemntation in angular 1.4 simply changed

--- app/components/settings/settings-service.js
+++ app/components/settings/settings-service.js
@@ -6,17 +6,17 @@ angular.module('myApp.settings', ['ngCookies'])
 .factory('WCSettings', [ '$cacheFactory', '$cookies', function($cacheFactory, $cookies) {
   var settingsCache = $cacheFactory('wcsettings');
   var hostId = 'hostId';
-  var cookieName = 'wcCookie';
+  var cookieName = 'wc-cookie';
   return {
     setHost: function(host) {
       settingsCache.put(hostId, host);
-      console.log('Setting cookie: ', host);
-      $cookies.wcCookie = host;
+      // console.log('Setting cookie: ', host);
+      $cookies.put(cookieName, host);
     },
     getHost: function() {
       var host = settingsCache.get(hostId);
       if (!host) {
-        return $cookies.wcCookie; // "http://localhost";
+        return $cookies.get(cookieName) || "http://localhost";
       } else {
         return host;
       }
