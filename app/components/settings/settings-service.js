3593b0bc3c07ab52b8da2ec72045dd500fbd1fd0
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-03-14 22:49:06 +0100

    Apparently, angular 1.4 has broken

--- app/components/settings/settings-service.js
+++ app/components/settings/settings-service.js
@@ -2,18 +2,21 @@
 
 'use strict';
 
-angular.module('myApp.settings', [])
-.factory('WCSettings', [ '$cacheFactory', function($cacheFactory) {
+angular.module('myApp.settings', ['ngCookies'])
+.factory('WCSettings', [ '$cacheFactory', '$cookies', function($cacheFactory, $cookies) {
   var settingsCache = $cacheFactory('wcsettings');
   var hostId = 'hostId';
+  var cookieName = 'wcCookie';
   return {
     setHost: function(host) {
       settingsCache.put(hostId, host);
+      console.log('Setting cookie: ', host);
+      $cookies.wcCookie = host;
     },
     getHost: function() {
       var host = settingsCache.get(hostId);
       if (!host) {
-        return "http://localhost";
+        return $cookies.wcCookie; // "http://localhost";
       } else {
         return host;
       }
